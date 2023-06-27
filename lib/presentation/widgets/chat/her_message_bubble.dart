aa432555788d5c40443b8a723ff2db53183d0f4d
AgnosticSix <mateo_godfather@hotmail.com>
2023-06-27 00:59:49 -0600

    added her messages and showed them in chat_screen

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -5,6 +5,37 @@ class HerMessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+
+        //_ImageBubble()
+      ],
+    );
+  }
+}
+
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
