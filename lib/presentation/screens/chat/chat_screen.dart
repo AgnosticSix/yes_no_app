b623c14ac87a55de374767a6fea06c048a8dac97
AgnosticSix <mateo_godfather@hotmail.com>
2023-06-27 00:04:11 -0600

    first commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://c-cl.cdn.smule.com/rs-s25/arr/cc/43/5275d7e4-4c47-4827-9a42-6d51ff322e4c.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor 😍'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index ) {
+                      return Text('Indice: $index');
+                    })),
+            const Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
