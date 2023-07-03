50e91a556a3a2552bb5251e2f00af52db6aa4edd
AgnosticSix <mateo_godfather@hotmail.com>
2023-07-02 23:02:30 -0600

    autoscroll to the end of the listview added

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola amor!', fromWhom: FromWhom.me),
     Message(text: 'Ya regresaste del trabajo?', fromWhom: FromWhom.me)
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWhom: FromWhom.me);
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 150),
+        curve: Curves.easeOut);
   }
 }
